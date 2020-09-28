d4fcc8b80babc1576d8a1f9bb69d9553139210c7
NemethJohanna <nemethkjohanna@gmail.com>
2020-09-28 12:28:24 +0200

    created the empty fe and be projects

---
+++ Backend/src/main/java/com/hackathon/covidilemma/CovidilemmaApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.covidilemma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidilemmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidilemmaApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/hackathon/covidilemma/CovidilemmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.covidilemma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidilemmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
