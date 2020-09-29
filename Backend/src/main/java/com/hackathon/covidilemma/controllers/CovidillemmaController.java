81c09e9f479d9ef07b3732c3dfc1be6601243d8c
Eszter Viczian <eviczia@gmail.com>
2020-09-29 15:52:49 +0200

    merge commits (player-login + questions-quiz)

--- Backend/src/main/java/com/hackathon/covidilemma/controllers/CovidillemmaController.java
+++ Backend/src/main/java/com/hackathon/covidilemma/controllers/CovidillemmaController.java
@@ -1,9 +1,12 @@
 package com.hackathon.covidilemma.controllers;
 
+import com.hackathon.covidilemma.models.dtos.NewPlayer;
 import com.hackathon.covidilemma.services.CovidilemmaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- Backend/src/main/java/com/hackathon/covidilemma/services/CovidilemmaService.java
+++ Backend/src/main/java/com/hackathon/covidilemma/services/CovidilemmaService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface CovidilemmaService {
 
   void addNewPlayer(String name);
+
   List<Question> getQuestions();
 
 }
