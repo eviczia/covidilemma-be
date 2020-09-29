54bb72764466f87153f710694b33977e5a44a278
Eszter Viczian <eviczia@gmail.com>
2020-09-29 10:30:21 +0200

    link db

--- Backend/src/main/java/com/hackathon/covidilemma/models/entities/Game.java
+++ Backend/src/main/java/com/hackathon/covidilemma/models/entities/Game.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
 
