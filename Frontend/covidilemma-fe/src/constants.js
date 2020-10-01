321c82fc4e9a6d0effcacc9318f3b45a6f8570fb
Eszter Viczian <eviczia@gmail.com>
2020-10-01 17:43:46 +0200

    delete fe

--- Frontend/covidilemma-fe/src/constants.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const URL = "https://localhost:3000";
-export const API_URL = "https://localhost:3000";
-export const my_app = axios.create({ baseURL: API_URL });
